9070a8277b2a9337c109da5041f8f2034515bea3
ThongHT <thonght150201@gmail.com>
2023-05-14 22:14:40 +0700

    authen login

---
+++ BirdClubAPI.BusinessLayer/Services/Auth/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using BirdClubAPI.DataAccessLayer.Repositories.User;
+using BirdClubAPI.Domain.Commons.Utils;
+using BirdClubAPI.Domain.DTOs.Request.Auth;
+using BirdClubAPI.Domain.DTOs.View.Auth;
+using BirdClubAPI.Domain.DTOs.View.Common;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+
+namespace BirdClubAPI.BusinessLayer.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IUserRepository userRepository, IMapper mapper, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        public KeyValuePair<MessageViewModel, AuthViewModel?> Login(LoginFormRequestModel loginFormRequest)
+        {
+            var user = _userRepository.Get(loginFormRequest.Email, loginFormRequest.Password);
+            if (user == null)
+            {
+                return new KeyValuePair<MessageViewModel, AuthViewModel?>(
+                new MessageViewModel { StatusCode = HttpStatusCode.Unauthorized, Message = "Wrong user name or password" },
+                null
+                );
+            }
+            var viewModel = new KeyValuePair<MessageViewModel, AuthViewModel?>(
+                new MessageViewModel { StatusCode = HttpStatusCode.OK, Message = "Authenticated" },
+                _mapper.Map<AuthViewModel>(user)
+                );
+            var jwtToken = TokenManager.GenerateJwtToken(user.Email, user.DisplayName, user.UserType, user.Id, _configuration);
+            viewModel.Value.JwtToken = jwtToken;
+            return viewModel;
+        }
+    }
+}
---
+++ BirdClubAPI.DataAccessLayer/Repositories/User/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using BirdClubAPI.DataAccessLayer.Context;
+
+namespace BirdClubAPI.DataAccessLayer.Repositories.User
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly BirdClubContext _context;
+
+        public UserRepository(BirdClubContext context)
+        {
+            _context = context;
+        }
+
+        public Domain.Entities.User? Get(string email, string password)
+        {
+            var user = _context.Users.SingleOrDefault(e => e.Email.Equals(email) && e.Password.Equals(password));
+            return user;
+        }
+    }
+}
